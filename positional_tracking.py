adea0c75cb6988711eeae00ba71573705a8b22af
Hayden Shively <haydenshively@gmail.com>
2018-01-07 20:59:38 -0600

    Implemented streaming of pertinent data to NT tables

--- positional_tracking.py
+++ positional_tracking.py
@@ -17,7 +17,7 @@ def default_init_params():
     params.coordinate_units = sl.PyUNIT.PyUNIT_METER
     return params
 
-def stream_position():
+def stream_position(to):
     zed = zcam.PyZEDCamera()
     init_params = default_init_params()
     exit_if_failure(zed.open(init_params))# opens camera and checks for errors at the same time
@@ -26,37 +26,41 @@ def stream_position():
     tracking_params = zcam.PyTrackingParameters(init_pos = transform)
     exit_if_failure(zed.enable_tracking(tracking_params))# enables tracking and checks for errors at the same time
 
-    # Track the camera position during 1000 frames
-    i = 0
+    translationBin = to.getSubTable('Translation')
+    # rotationBin = to.getSubTable('Rotation')
+
     zed_pose = zcam.PyPose()
-    while i < 1000:
-        if zed.grab(zcam.PyRuntimeParameters()) == tp.PyERROR_CODE.PySUCCESS:
-            # Get the pose of the left eye of the camera with reference to the world frame
-            zed.get_position(zed_pose, sl.PyREFERENCE_FRAME.PyREFERENCE_FRAME_WORLD)
+    while True:
+        try:
+            if zed.grab(zcam.PyRuntimeParameters()) == tp.PyERROR_CODE.PySUCCESS:
+                # Get the pose of the left eye of the camera with reference to the world frame
+                zed.get_position(zed_pose, sl.PyREFERENCE_FRAME.PyREFERENCE_FRAME_WORLD)
 
-            # Display the translation and timestamp
-            py_translation = core.PyTranslation()
-            tx = round(zed_pose.get_translation(py_translation).get()[0], 2)
-            ty = round(zed_pose.get_translation(py_translation).get()[1], 2)
-            tz = round(zed_pose.get_translation(py_translation).get()[2], 2)
-            print("Translation: Tx: {0}, Ty: {1}, Tz {2}, Timestamp: {3}\n".format(tx, ty, tz, zed_pose.timestamp))
+                translation = core.PyTranslation()
+                tx = round(zed_pose.get_translation(translation).get()[0], 2)
+                ty = round(zed_pose.get_translation(translation).get()[1], 2)
+                tz = round(zed_pose.get_translation(translation).get()[2], 2)
+                translationBin.putNumber('x', tx)
+                translationBin.putNumber('y', ty)
+                translationBin.putNumber('z', tz)
 
-            # Display the orientation quaternion
-            #py_orientation = core.PyOrientation()
-            #ox = round(zed_pose.get_orientation(py_orientation).get()[0], 3)
-            #oy = round(zed_pose.get_orientation(py_orientation).get()[1], 3)
-            #oz = round(zed_pose.get_orientation(py_orientation).get()[2], 3)
-            #ow = round(zed_pose.get_orientation(py_orientation).get()[3], 3)
-            #print("Orientation: Ox: {0}, Oy: {1}, Oz {2}, Ow: {3}\n".format(ox, oy, oz, ow))
+                # orientation = core.PyOrientation()
+                # ox = round(zed_pose.get_orientation(orientation).get()[0], 3)
+                # oy = round(zed_pose.get_orientation(orientation).get()[1], 3)
+                # oz = round(zed_pose.get_orientation(orientation).get()[2], 3)
+                # ow = round(zed_pose.get_orientation(orientation).get()[3], 3)
+                # rotationBin.putNumber('x', ox)
+                # rotationBin.putNumber('y', oy)
+                # rotationBin.putNumber('z', oz)
+                # rotationBin.putNumber('w', ow)
 
-            i = i + 1
-
-    # Close the camera
-    zed.close()
+                to.putNumber('timestamp', zed_pose.timestamp)
+        except:
+            zed.close()
+            break
 
 if __name__ == "__main__":
     rioURL = '192.168.1.87'#'roborio-4256-frc.local'
     NetworkTables.initialize(server = rioURL)
-    portal = NetworkTables.getTable('ZED')
-    portal.putBoolean("test", True)
-    stream_position()
+    table = NetworkTables.getTable('ZED')
+    stream_position(to = table)
