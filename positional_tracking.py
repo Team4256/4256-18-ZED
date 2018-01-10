ea91409f5480335ffb7e765cd2b8a2d31049843b
Hayden Shively <haydenshively@gmail.com>
2018-01-09 22:07:42 -0600

    Forced to debug. Sadly every change goes through GitHub

--- positional_tracking.py
+++ positional_tracking.py
@@ -11,22 +11,22 @@ def stream_position(to):
     positionBin = to.getSubTable('Position')
 
     while True:
-        try:
-            zed.grab()
-            new_position = zed.position()
-            if new_position is not None:
-                positionBin.putNumber('X', round(new_position[0], 3))
-                positionBin.putNumber('Y', round(new_position[1], 3))
-                positionBin.putNumber('Z', round(new_position[2], 3))
+        # try:
+        zed.grab()
+        new_position = zed.position()
+        if new_position is not None:
+            positionBin.putNumber('X', round(new_position[0], 3))
+            positionBin.putNumber('Y', round(new_position[1], 3))
+            positionBin.putNumber('Z', round(new_position[2], 3))
 
-                positionBin.putNumber('Timestamp', zed.pose.timestamp/1e13)
-                positionBin.putString('Tracking Status', zed.tracking_status)
+            positionBin.putNumber('Timestamp', zed.pose.timestamp/1e13)
+            positionBin.putString('Tracking Status', zed.tracking_status)
 
-            to.putString('Overall Status', zed.overall_status)
+        to.putString('Overall Status', zed.overall_status)
 
-        except:
-            zed.camera.close()
-            break
+        # except:
+        #     zed.camera.close()
+        #     break
 
 if __name__ == '__main__':
     rioURL = '192.168.0.195'#'roborio-4256-frc.local'
