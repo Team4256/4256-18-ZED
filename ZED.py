d281eb91c90e3d517acc7fac6274dc5f52ceda95
Hayden Shively <haydenshively@gmail.com>
2018-01-09 21:36:04 -0600

    Vastly simplified

--- ZED.py
+++ ZED.py
@@ -1,3 +1,24 @@
+"""
+PySUCCESS : standard code
+PyERROR_CODE_FAILURE : standard code
+PyERROR_CODE_NO_GPU_COMPATIBLE
+PyERROR_CODE_NOT_ENOUGH_GPUMEM
+PyERROR_CODE_CAMERA_NOT_DETECTED
+PyERROR_CODE_INVALID_RESOLUTION
+PyERROR_CODE_LOW_USB_BANDWIDTH : occurs when using ZED through USB 2.0
+PyERROR_CODE_CALIBRATION_FILE_NOT_AVAILABLE : use ZED Explorer to make one
+PyERROR_CODE_INVALID_SVO_FILE
+PyERROR_CODE_SVO_RECORDING_ERROR
+PyERROR_CODE_INVALID_COORDINATE_SYSTEM
+PyERROR_CODE_INVALID_FIRMWARE
+PyERROR_CODE_NOT_A_NEW_FRAME : specific to grab()
+PyERROR_CODE_CUDA_ERROR : specific to grab()
+PyERROR_CODE_CAMERA_NOT_INITIALIZED : specific to grab(), fix by calling open()
+PyERROR_CODE_NVIDIA_DRIVER_OUT_OF_DATE
+PyERROR_CODE_INVALID_FUNCTION_CALL : usually fix by calling open()
+PyERROR_CODE_CORRUPTED_SDK_INSTALLATION
+PyERROR_CODE_LAST
+"""
 #{ZED}
 import pyzed.camera as zcam
 import pyzed.defines as sl
@@ -11,16 +32,40 @@ def default_init_params():
     params.coordinate_units = sl.PyUNIT.PyUNIT_METER
     return params
 
+
 class ZED(object):
-    def __init__(self, error_handler = exit):
+    def __init__(self):
         self.camera = zcam.PyZEDCamera()
-        def check_status(operation):
-            if operation != tp.PyERROR_CODE.PySUCCESS:
-                error_handler(sl.errorCode2str(operation))
-        self.check_status = check_status
-        self.check_status(self.camera.open(default_init_params()))# opens camera and checks for errors at the same time
+        self.overall_status = self.camera.open(default_init_params())# opens camera and updates overall_status at the same time
+        self.pose = None# just declaring for later use
 
     def enable_tracking(self):
         transform = core.PyTransform()
         params = zcam.PyTrackingParameters(init_pos = transform)
-        self.check_status(self.camera.enable_tracking(params))# enables tracking and checks for errors at the same time
+        self.overall_status = self.camera.enable_tracking(params)# enables tracking and updates overall_status at the same time
+        self.pose = zcam.PyPose()
+
+    def grab(self):
+        self.overall_status = self.camera.grab(zcam.PyRuntimeParameters())
+        if self._overall_status == tp.PyERROR_CODE.PySUCCESS:
+            self.camera.get_position(self.pose, sl.PyREFERENCE_FRAME.PyREFERENCE_FRAME_WORLD)# locates left camera eye with respect to world
+
+    def position(self):
+        if self._overall_status == tp.PyERROR_CODE.PySUCCESS:
+            return self.pose.get_translation(core.PyTranslation()).get()
+        else:
+            return None
+
+    def orientation(self):
+        if self._overall_status == tp.PyERROR_CODE.PySUCCESS:
+            return self.pose.get_orientation(core.PyOrientation()).get()
+        else:
+            return None
+
+    @property# this denotes a "getter," meaning the function below generates the value of self.overall_status
+    def overall_status(self):
+        return sl.errorCode2str(self._overall_status)
+
+    @overall_status.setter# this denotes a "setter," meaning the function below assigns a new value to self.overall_status
+    def overall_status(self, update):
+        self._overall_status = update
--- positional_tracking.py
+++ positional_tracking.py
@@ -5,45 +5,30 @@ from networktables import NetworkTables
 
 
 def stream_position(to):
-    def nt_and_exit(message):
-        to.putString('Error', message)
-        exit(message)
-    zed = ZED(error_handler = nt_and_exit)
+    zed = ZED()
     zed.enable_tracking()
 
     translationBin = to.getSubTable('Translation')
     # rotationBin = to.getSubTable('Rotation')
 
-    zed_pose = zcam.PyPose()
     while True:
-        to.putBoolean('Running', True)
         try:
-            if zed.grab(zcam.PyRuntimeParameters()) == tp.PyERROR_CODE.PySUCCESS:
-                # Get the pose of the left eye of the camera with reference to the world frame
-                zed.get_position(zed_pose, sl.PyREFERENCE_FRAME.PyREFERENCE_FRAME_WORLD)
+            zed.grab()
+            new_position = zed.position()
+            if new_position is not None:
+                translationBin.putNumber('X', round(new_position[0], 3))
+                translationBin.putNumber('Y', round(new_position[1], 3))
+                translationBin.putNumber('Z', round(new_position[2], 3))
 
-                translation = core.PyTranslation()
-                tx = round(zed_pose.get_translation(translation).get()[0], 2)
-                ty = round(zed_pose.get_translation(translation).get()[1], 2)
-                tz = round(zed_pose.get_translation(translation).get()[2], 2)
-                translationBin.putNumber('X', tx)
-                translationBin.putNumber('Y', ty)
-                translationBin.putNumber('Z', tz)
+            # new_orientation = zed.orientation()
+            #     rotationBin.putNumber('X', round(new_position[0], 2))
+            #     rotationBin.putNumber('Y', round(new_position[1], 2))
+            #     rotationBin.putNumber('Z', round(new_position[2], 2))
+            #     rotationBin.putNumber('W', round(new_position[3], 2))
 
-                # orientation = core.PyOrientation()
-                # ox = round(zed_pose.get_orientation(orientation).get()[0], 3)
-                # oy = round(zed_pose.get_orientation(orientation).get()[1], 3)
-                # oz = round(zed_pose.get_orientation(orientation).get()[2], 3)
-                # ow = round(zed_pose.get_orientation(orientation).get()[3], 3)
-                # rotationBin.putNumber('X', ox)
-                # rotationBin.putNumber('Y', oy)
-                # rotationBin.putNumber('Z', oz)
-                # rotationBin.putNumber('W', ow)
-
-                to.putNumber('Timestamp', zed_pose.timestamp/1e13)
+                # to.putNumber('Timestamp', zed_pose.timestamp/1e13)
         except:
-            to.putBoolean('Running', False)
-            zed.close()
+            zed.camera.close()
             break
 
 if __name__ == "__main__":
