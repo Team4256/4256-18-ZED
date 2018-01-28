1379d98318a34741d898d4f85df3e81e27b99af0
Hayden Shively <haydenshively@gmail.com>
2018-01-28 22:03:46 +0000

    Added video and point cloud getters to the ZED camera class.

--- Tracking.py
+++ Tracking.py
@@ -2,11 +2,15 @@
 from ZED import ZED
 #{RobotPy}
 from networktables import NetworkTables
+#{OpenCV}
+import cv2
 
 
 def stream_position(to):
     zed = ZED()
     zed.enable_tracking()
+    zed.enable_video()
+    zed.enable_cloud()
 
     positionBin = to.getSubTable('Position')
 
@@ -19,19 +23,26 @@ def stream_position(to):
                 positionBin.putNumber('Y', round(new_position[1], 3))
                 positionBin.putNumber('Z', round(new_position[2], 3))
 
-                positionBin.putNumber('Timestamp', zed.pose.timestamp/1e13)
+                #positionBin.putNumber('Timestamp', zed.pose.timestamp/1e13)
                 positionBin.putString('Tracking Status', zed.tracking_status)
+
+            new_image = zed.numpy_image()
+            if new_image is not None:
+                cv2.imshow('Live', new_image)
+                cv2.waitKey(5)
+
+            new_cloud = zed.numpy_cloud()
 
             to.putString('Overall Status', zed.overall_status)
 
-        except:
+        except KeyboardInterrupt:
             zed.camera.close()
             break
 
 if __name__ == '__main__':
-    rioURL = '192.168.0.195'#'roborio-4256-frc.local'
+    rioURL = '192.168.0.189'#'10.42.56.2'
     NetworkTables.initialize(server = rioURL)
-    #NetworkTables.startClientTeam(4256)
+    #NetworkTables.startClientTeam(4256, port = 1735)
     NetworkTables.setUpdateRate(.020)
     stream_position(to = NetworkTables.getTable('ZED'))
     NetworkTables.stopClient()
--- ZED.py
+++ ZED.py
@@ -24,49 +24,119 @@ import pyzed.camera as zcam
 import pyzed.defines as sl
 import pyzed.types as tp
 import pyzed.core as core
+#{numpy}
+from numpy import ctypeslib
 
 def default_init_params():
     params = zcam.PyInitParameters()# creates a place to store param
-    params.camera_resolution = sl.PyRESOLUTION.PyRESOLUTION_HD720# HD720, 60 fps
+    params.camera_resolution = sl.PyRESOLUTION.PyRESOLUTION_HD720
+    params.camera_fps = 60
+    # options are HD2K (15fps), HD1080 (max 30fps), HD720 (max 60fps), WVGA (max 100fps)
+    params.depth_mode = sl.PyDEPTH_MODE.PyDEPTH_MODE_PERFORMANCE
+    # options are NONE, PERFORMANCE, MEDIUM, QUALITY, ULTRA
+    params.depth_stabilization = True
+    params.depth_minimum_distance = 7
+    # less than 10, lower numbers increase grab() time
     params.coordinate_system = sl.PyCOORDINATE_SYSTEM.PyCOORDINATE_SYSTEM_RIGHT_HANDED_Z_UP
-    params.coordinate_units = sl.PyUNIT.PyUNIT_METER
+    params.coordinate_units = sl.PyUNIT.PyUNIT_FOOT
+    # options are MILLIMETER, CENTIMETER, METER, INCH, FOOT
+    return params
+
+def default_tracking_params():
+    params = zcam.PyTrackingParameters(init_pos = core.PyTransform())
+    params.enable_spatial_memory = True
+    #params.enable_pose_smoothing = False # Listed in documentation but doesn't work
     return params
 
 
 class ZED(object):
     def __init__(self):
         self.camera = zcam.PyZEDCamera()
-        #self.pose = None# just declaring for later use
-        self.overall_status = self.camera.open(default_init_params())# opens camera and updates overall_status at the same time
-        self.tracking_status = "Disabled"
+        self.overall_status = self.camera.open(default_init_params())
+        # opens camera and updates overall_status at the same time
+        self.tracking_status = 'Disabled'
+        self.video_status = 'Disabled'
+        self.cloud_status = 'Disabled'
 
-    def enable_tracking(self):
-        params = zcam.PyTrackingParameters(init_pos = core.PyTransform())
-        self.overall_status = self.camera.enable_tracking(params)# enables tracking and updates overall_status at the same time
+    """load_area should be False or a file path string"""
+    def enable_tracking(self, load_area = False):
+        params = default_tracking_params()
+        if load_area is not False:
+            params.area_file_path = load_area
+        self.overall_status = self.camera.enable_tracking(params)
+        # enables tracking and updates overall_status at the same time
         self.pose = zcam.PyPose()
+        self.tracking_status = 'Enabled'
+
+    """save_area should be False or a file path string"""
+    def disable_tracking(self, save_area = False):
+        if save_area is not False:
+            self.camera.disable_tracking(save_area)
+        else:
+            self.camera.disable_tracking()
+        self.tracking_status = 'Disabled'
+
+    def enable_video(self):
+        self.image = core.PyMat()
+        self.video_status = 'Enabled'
+
+    def disable_video(self):
+        self.video_status = 'Disabled'
+
+    def enable_cloud(self):
+        self.cloud = core.PyMat()
+        self.cloud_status = 'Enabled'
+
+    def disable_cloud(self):
+        self.cloud_status = 'Disabled'
 
     def grab(self):
         self.overall_status = self.camera.grab(zcam.PyRuntimeParameters())
         if self._overall_status == tp.PyERROR_CODE.PySUCCESS:
-            self.tracking_status = self.camera.get_position(self.pose, sl.PyREFERENCE_FRAME.PyREFERENCE_FRAME_WORLD)# locates left camera eye with respect to world
+            if self.tracking_status is not 'Disabled':
+                self.tracking_status = self.camera.get_position(self.pose, sl.PyREFERENCE_FRAME.PyREFERENCE_FRAME_WORLD)
+                # locates left camera with respect to world
+            if self.video_status is not 'Disabled':
+                self.video_status = self.camera.retrieve_image(self.image, sl.PyVIEW.PyVIEW_LEFT)#, core.PyMEM.PyMEM_CPU, width = 0, height = 0)
+                # GPU mode doesn't work, width and height of 0 means default
+                # from left camera
+            if self.cloud_status is not 'Disabled':
+                self.cloud_status = self.camera.retrieve_measure(self.cloud, sl.PyMEASURE.PyMEASURE_XYZBGRA)#, core.PyMEM.PyMEM_CPU, width = 0, height = 0)
+                # GPU mode doesn't work, width and height of 0 means default
+                # from left camera
 
     def position(self):
-        if self._overall_status == tp.PyERROR_CODE.PySUCCESS:
+        if self._tracking_status == sl.PyTRACKING_STATE.PyTRACKING_STATE_OK:
             return self.pose.get_translation(core.PyTranslation()).get()
         else:
             return None
 
     def orientation(self):
-        if self._overall_status == tp.PyERROR_CODE.PySUCCESS:
+        if self._tracking_status == sl.PyTRACKING_STATE.PyTRACKING_STATE_OK:
             return self.pose.get_orientation(core.PyOrientation()).get()
         else:
             return None
 
-    @property# this denotes a "getter," meaning the function below generates the value of self.overall_status
+    def numpy_image(self):
+        if self._video_status == tp.PyERROR_CODE.PySUCCESS:
+            return self.image.get_data()
+        else:
+            return None
+
+    def numpy_cloud(self):
+        if self._cloud_status == tp.PyERROR_CODE.PySUCCESS:
+            return self.cloud.get_data()
+        else:
+            return None
+
+
+    @property
+    # this denotes a "getter," meaning the function below generates the value of self.overall_status
     def overall_status(self):
         return str(self._overall_status)
 
-    @overall_status.setter# this denotes a "setter," meaning the function below assigns a new value to self.overall_status
+    @overall_status.setter
+    # this denotes a "setter," meaning the function below assigns a new value to self.overall_status
     def overall_status(self, update):
         self._overall_status = update
 
@@ -77,3 +147,20 @@ class ZED(object):
     @tracking_status.setter
     def tracking_status(self, update):
         self._tracking_status = update
+
+    @property
+    def video_status(self):
+        return str(self._video_status)
+
+    @video_status.setter
+    def video_status(self, update):
+        self._video_status = update
+
+    @property
+    def cloud_status(self):
+        return str(self._cloud_status)
+
+    @cloud_status.setter
+    def cloud_status(self, update):
+        self._cloud_status = update
+
