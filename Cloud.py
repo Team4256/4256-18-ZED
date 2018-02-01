221520893452eead5f75bc9f44b468606b8ae29a
Hayden Shively <haydenshively@gmail.com>
2018-01-31 20:36:44 -0600

    Created a pretty simple function to take 2D depth maps and produce the top down views we want. Changes color based on Y values of original, but doesn't take into account the fact that some Y values are impossible as Z increases

--- Cloud.py
+++ Cloud.py
@@ -1,10 +1,10 @@
-"""https://github.com/VincentCheungM/lidar_projection/blob/master/show.py"""
+'''https://github.com/VincentCheungM/lidar_projection/blob/master/show.py'''
 import numpy as np
 
 def scale_to_255(array, minimum, maximum):
     return (255*(array - minimum)/float(maximum - minimum)).astype(array.dtype)
 
-def birds_eye_projection(cloud, x_range = (-10, 10), y_range = (-10, 10), z_range = (-3, 3), res = 1):
+def birds_eye_projection(cloud, x_range = (-100, 100), y_range = (-100, 100), z_range = (-100, 100), res = 1.0):
     rows, columns = cloud.shape[:2]
     cloud = cloud.reshape((rows*columns, -1))#.astype('int32')
     cloudX, cloudY, cloudZ, color = np.split(cloud, 4, axis = 1)# 4 to get color too
@@ -39,5 +39,31 @@ def birds_eye_projection(cloud, x_range = (-10, 10), y_range = (-10, 10), z_rang
     img[-imgY.astype('int32'), imgX.astype('int32')] = pixel_values
     return img
 
-cloud = np.load("cloud.npy")
-print(birds_eye_projection(cloud))
+
+
+def get_top_from_front(depth_map, section_count = 127):
+    section_size = depth_map.ptp()/section_count# max - min
+    rows, columns = depth_map.shape[:2]
+    sections = np.zeros((rows, columns, section_count), dtype = 'uint32')
+    for i in range(section_count):
+        indices = np.logical_and((depth_map >= i*section_size), (depth_map < (i+1)*section_size))
+        sections[indices, i] = 1#depth_map[indices]
+    y_mult = np.arange(0, rows, dtype = 'uint32')[:, None, None]
+    sections *= y_mult
+    return sections.mean(axis = 0).transpose()
+
+
+if __name__ == '__main__':
+    '''This is meant to be used for debugging purposes; works with any depth map image.'''
+    import cv2
+    depth_map = cv2.imread('depth.jpg', 0)# get grayscale depth map
+    result_width = depth_map.shape[1]
+    result_height = 200
+    hsv = np.full((result_height, result_width, 3), 255, dtype = 'uint8')# prepare empty array for colored result
+    top_view = get_top_from_front(depth_map)
+    top_view *= 255/top_view.max()# scales to make new max() 255
+    result = cv2.resize(top_view.astype('uint8'), (result_width, result_height), interpolation = cv2.INTER_LINEAR)# stretch the height
+    hsv[:,:,0] = result
+    cv2.imshow('Depth Map', depth_map)
+    cv2.imshow('Top View', cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR))
+    cv2.waitKey(0)
