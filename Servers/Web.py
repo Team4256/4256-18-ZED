bec1a60d206e2b1a960bf1c4ed2ca017e12fd681
Hayden Shively <haydenshively@gmail.com>
2018-04-19 16:59:38 -0500

    Added try, except for annoying BrokenPipeError

--- Servers/Web.py
+++ Servers/Web.py
@@ -14,60 +14,63 @@ class ImageHandler(BaseHTTPRequestHandler):
     enabled = False
 
     def do_GET(self):
-        if self.path.endswith(".mjpg"):
-            self.send_response(200)
-            self.send_header("Content-type", "multipart/x-mixed-replace; boundary=--jpgboundary")
-            self.end_headers()
-            while self.enabled:
-                image = self.stitched_queue.get(True)
-                while True:
-                    try:
-                        image = self.stitched_queue.get_nowait()
-                    except Empty:
-                        break
-
-
-                image_jpg = cv2.imencode('.jpeg', image)[1]
-                self.wfile.write("--jpgboundary".encode())
-                self.send_header("Content-type", "image/jpeg")
-                self.send_header("Content-length", str(image_jpg.size))
+        try:
+            if self.path.endswith(".mjpg"):
+                self.send_response(200)
+                self.send_header("Content-type", "multipart/x-mixed-replace; boundary=--jpgboundary")
                 self.end_headers()
-                self.wfile.write(image_jpg.tostring())
+                while self.enabled:
+                    image = self.stitched_queue.get(True)
+                    while True:
+                        try:
+                            image = self.stitched_queue.get_nowait()
+                        except Empty:
+                            break
 
 
-        if self.path.endswith('.html'):
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write("""
-                <html>
-                  <head>
-                  </head>
-                  <body>
-                    <img src="/canvas.mjpg" style="position: absolute;margin-left: 25%;top: 25%;" name="image"/>
-                    <script type="text/javascript">
-
-                    var updateSource = new EventSource("/updates.gyro");
-
-                    updateSource.onmessage = function(event) {image.style.transform = "rotate(" + event.data.toString() + "deg)";};
-
-                    </script>
-                  </body>
-                </html>
-                """.encode())
+                    image_jpg = cv2.imencode('.jpeg', image)[1]
+                    self.wfile.write("--jpgboundary".encode())
+                    self.send_header("Content-type", "image/jpeg")
+                    self.send_header("Content-length", str(image_jpg.size))
+                    self.end_headers()
+                    self.wfile.write(image_jpg.tostring())
 
 
-        if self.path.endswith('.gyro'):
-            self.send_response(200)
-            self.send_header("Content-type", "text/event-stream")
-            self.send_header("Cache-Control", "no-cache")
-            self.end_headers()
-            while self.enabled:
-                # self.rotation = (self.rotation+1)%360
-                # self.wfile.write("data: {}\n\n".format(self.rotation).encode())
-                gyro_angle = 0.0#self.robot_data.getNumber('Gyro', 0.0)
-                self.wfile.write("data: {}\n\n".format(gyro_angle).encode())
-                sleep(0.6)
+            if self.path.endswith('.html'):
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write("""
+                    <html>
+                      <head>
+                      </head>
+                      <body>
+                        <img src="/canvas.mjpg" style="position: absolute;margin-left: 25%;top: 25%;" name="image"/>
+                        <script type="text/javascript">
+
+                        var updateSource = new EventSource("/updates.gyro");
+
+                        updateSource.onmessage = function(event) {image.style.transform = "rotate(" + event.data.toString() + "deg)";};
+
+                        </script>
+                      </body>
+                    </html>
+                    """.encode())
+
+
+            if self.path.endswith('.gyro'):
+                self.send_response(200)
+                self.send_header("Content-type", "text/event-stream")
+                self.send_header("Cache-Control", "no-cache")
+                self.end_headers()
+                while self.enabled:
+                    # self.rotation = (self.rotation+1)%360
+                    # self.wfile.write("data: {}\n\n".format(self.rotation).encode())
+                    gyro_angle = 0.0#self.robot_data.getNumber('Gyro', 0.0)
+                    self.wfile.write("data: {}\n\n".format(gyro_angle).encode())
+                    sleep(0.6)
+                    
+        except BrokenPipeError: pass
 
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
